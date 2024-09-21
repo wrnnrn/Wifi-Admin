6509a16c28e36c278e105c9ba5e44171d3e6d184
wrnnrn <hugogimero@gmail.com>
2024-09-22 00:07:09 +0200

    Dev branch

--- client-server/client-server.py
+++ client-server/client-server.py
@@ -1,6 +1,1 @@
-import socket
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # AF_INET = Specificate the directions family (IPv4) SOCK_STREAM = TCP Communication
-
-def run_server():
-
+print("Client-server archive Dev")
