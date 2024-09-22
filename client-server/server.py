cc878dfffd8c82734c61a29065710f44734a399b
wrnnrn <hugogimero@gmail.com>
2024-09-22 08:33:52 +0200

    Send and recive message

--- client-server/server.py
+++ client-server/server.py
@@ -13,7 +13,19 @@ while True:
     print("Waiting connection")
     # The server accept the client
     connection, client_address = sock.accept()
+    
     try:
         print('Connection from', client_address)
+    
+        while True:
+            data = connection.recv(16)
+            print('received {!r}'.format(data))
+            if data:
+                print("Returning data to the client")
+                connection.sendall(data)
+            else:
+                print('no data from', client_address)
+                break
+
     finally:
         connection.close()
